aece17244618a54bafdc48b40295e909277460d5
Piyush M <mpiyush15@gmail.com>
2026-04-12 10:12:15 +0530

    Disable ESLint rule for unescaped entities in legal pages

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+/* eslint-disable react/no-unescaped-entities */
+
 export default function Contact() {
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
